3f73e66d132ceeaa8b440174d738d51daf5c7dc6
Joshua Malemba <joshmalemba@gmail.com>
2021-08-13 13:51:07 +0100

    changes

--- webHook.py
+++ webHook.py
@@ -2,8 +2,11 @@ import requests
 import json
 
 local_url = 'http://4f9836ac50b3.ngrok.io/webhook'
+local_url2 = 'http://4f9836ac50b3.ngrok.io/file'
 
 data = {'Device': 'CSI-Connect',
          'Hydro Pump State': 'Good'}
 
 local = requests.post(local_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+local2 = requests.post(local_url2, data=json.dumps(data), headers={'Content-Type': 'application/json'})
